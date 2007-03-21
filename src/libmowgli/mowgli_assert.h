8b5238a85aade7d00082d5432c6e853e75f18f31
William Pitcock <nenolod@chocobo.sacredspiral.co.uk>
2007-03-21 03:57:05 -0500

    Remove GNU C ifdefs from here, as it's not needed.
    committer: William Pitcock <nenolod@chocobo.sacredspiral.co.uk>

--- src/libmowgli/mowgli_assert.h
+++ src/libmowgli/mowgli_assert.h
@@ -37,50 +37,27 @@
 /*
  * Performs a soft assertion. If the assertion fails, we log it.
  */
-#ifdef __GNUC__
 #define soft_assert(x)								\
 	if (!(x)) { 								\
 		mowgli_log("critical: Assertion '%s' failed.", #x);		\
 	}
-#else
-#define soft_assert(x)								\
-	if (!(x)) { 								\
-		mowgli_log("critical: Assertion '%s' failed.", #x);		\
-	}
-#endif
 
 /*
  * Same as soft_assert, but returns if an assertion fails.
  */
-#ifdef __GNUC__
 #define return_if_fail(x)							\
 	if (!(x)) { 								\
 		mowgli_log("critical: Assertion '%s' failed.", #x);		\
 		return;								\
 	}
-#else
-#define return_if_fail(x)							\
-	if (!(x)) { 								\
-		mowgli_log("critical: Assertion '%s' failed.", #x);		\
-		return;								\
-	}
-#endif
 
 /*
  * Same as soft_assert, but returns a given value if an assertion fails.
  */
-#ifdef __GNUC__
 #define return_val_if_fail(x, y)						\
 	if (!(x)) { 								\
 		mowgli_log("critical: Assertion '%s' failed.", #x);		\
 		return (y);							\
 	}
-#else
-#define return_val_if_fail(x, y)						\
-	if (!(x)) { 								\
-		mowgli_log("critical: Assertion '%s' failed.", #x);		\
-		return (y);							\
-	}
-#endif
 
 #endif
