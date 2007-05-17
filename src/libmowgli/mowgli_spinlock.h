b51fc636382502d356ddcebd7a14266b46806b9d
William Pitcock <nenolod@petrie.sacredspiral.co.uk>
2007-05-17 15:47:22 -0500

    Add mowgli.spinlock library.
    committer: William Pitcock <nenolod@petrie.sacredspiral.co.uk>

--- src/libmowgli/mowgli_spinlock.h
+++ src/libmowgli/mowgli_spinlock.h
@@ -1,6 +1,6 @@
 /*
  * libmowgli: A collection of useful routines for programming.
- * mowgli.h: Base header for libmowgli. Includes everything.
+ * mowgli_spinlock.h: Spinlocks.
  *
  * Copyright (c) 2007 William Pitcock <nenolod -at- sacredspiral.co.uk>
  *
@@ -31,52 +31,24 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef __MOWGLI_STAND_H__
-#define __MOWGLI_STAND_H__
+#ifndef __MOWGLI_SPINLOCK_H__
+#define __MOWGLI_SPINLOCK_H__
 
-#ifdef __cplusplus
-# define MOWGLI_DECLS_START extern "C" {
-# define MOWGLI_DECLS_END   };
-#else
-# define MOWGLI_DECLS_START
-# define MOWGLI_DECLS_END
-#endif
+typedef struct {
+	void *read_owner;	/* opaque data representing a spinlock's owner */
+	void *write_owner;	/* opaque data representing a spinlock's owner */
+} mowgli_spinlock_t;
 
-#ifdef MOWGLI_CORE
-# include "mowgli_config.h"
-#endif
+typedef enum {
+	MOWGLI_SPINLOCK_READ,
+	MOWGLI_SPINLOCK_WRITE,
+	MOWGLI_SPINLOCK_READWRITE
+} mowgli_spinlock_lock_param_t;
 
-#include "mowgli_stdinc.h"
-
-MOWGLI_DECLS_START
-
-#include "mowgli_logger.h"
-#include "mowgli_assert.h"
-#include "mowgli_exception.h"
-
-#include "mowgli_alloc.h"
-#include "mowgli_spinlock.h"
-#include "mowgli_list.h"
-#include "mowgli_object_class.h"
-#include "mowgli_object.h"
-#include "mowgli_dictionary.h"
-#include "mowgli_mempool.h"
-#include "mowgli_module.h"
-#include "mowgli_queue.h"
-#include "mowgli_hash.h"
-#include "mowgli_heap.h"
-#include "mowgli_init.h"
-#include "mowgli_bitvector.h"
-#include "mowgli_hook.h"
-#include "mowgli_signal.h"
-#include "mowgli_error_backtrace.h"
-#include "mowgli_random.h"
-#include "mowgli_ioevent.h"
-#include "mowgli_argstack.h"
-#include "mowgli_object_messaging.h"
-#include "mowgli_object_metadata.h"
-
-MOWGLI_DECLS_END
+extern mowgli_spinlock_t *mowgli_spinlock_create(void);
+extern void mowgli_spinlock_lock(mowgli_spinlock_t *self, void *read, void *write);
+extern void mowgli_spinlock_unlock(mowgli_spinlock_t *self, void *read, void *write);
+extern void mowgli_spinlock_wait(mowgli_spinlock_t *self, mowgli_spinlock_lock_param_t param);
+extern void mowgli_spinlock_timed_wait(mowgli_spinlock_t *self, mowgli_spinlock_lock_param_t param, struct timeval *tv);
 
 #endif
-
