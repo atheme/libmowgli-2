bad44fbf5785a1b7d28bb04ef61d1f01b0bcfb8b
Elizabeth J. Myers <elizabeth@sporksmoo.net>
2012-03-19 02:51:39 -0500

    vio-udplistener: use IPv6 but a IPv6-mapped IPv4 address.
    This is sort of a test use case.

--- src/examples/vio-udplistener/vio-udplistener.c
+++ src/examples/vio-udplistener/vio-udplistener.c
@@ -7,12 +7,33 @@
 
 #define BUFSIZE 2048
 
+#define PROTO	AF_INET6
+#define LISTEN	"::ffff:127.0.0.1"	/* 6to4 mapping */
+#define PORT	31337
+
+char *get_ip_str(const struct sockaddr *addr, char *str, size_t len)
+{
+	void *ptr;
+
+	if (addr->sa_family == AF_INET)
+		ptr = &((struct sockaddr_in *)addr)->sin_addr;
+	else if (addr->sa_family == AF_INET6)
+		ptr = &((struct sockaddr_in6 *)addr)->sin6_addr;
+	else
+		return NULL;
+
+	if (inet_ntop(addr->sa_family, ptr, str, len) == NULL)
+		return NULL;
+
+	return str;
+}
+
 int main (void)
 {
 	mowgli_vio_t *vio = mowgli_vio_create(NULL);
-	mowgli_vio_sockaddr_t *addr = mowgli_sockaddr_create(AF_INET, NULL, 31337);
+	mowgli_vio_sockaddr_t *addr = mowgli_sockaddr_create(PROTO, LISTEN, 31337);
 
-	if (mowgli_vio_socket(vio, AF_INET, SOCK_DGRAM, 0))
+	if (mowgli_vio_socket(vio, PROTO, SOCK_DGRAM, 0))
 		return EXIT_FAILURE;
 
 	if (mowgli_vio_bind(vio, addr))
@@ -25,7 +46,7 @@ int main (void)
 
 		mowgli_vio_recvfrom(vio, buf, sizeof(buf), addr);
 
-		inet_ntop(AF_INET, &((struct sockaddr_in*)&addr->addr)->sin_addr, host, sizeof(host));
+		get_ip_str((struct sockaddr *)&addr->addr, host, sizeof(host));
 
 		printf("Recieved bytes from addr %s: %s", host, buf);
 
