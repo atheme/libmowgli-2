1d7b90669f92c831cd3d18932f1ce9d62c7bc111
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2012-03-18 20:18:20 -0400

    Update linetest example to match vio API change

--- src/examples/linetest/linetest.c
+++ src/examples/linetest/linetest.c
@@ -89,8 +89,8 @@ client_t * create_client(const char *server, const char *port, const char *nick,
 	mowgli_linebuf_attach_to_eventloop(linebuf, base_eventloop);
 
 	/* Do the connect */
-	linebuf->vio->addr = res->ai_addr;
-	linebuf->vio->addrlen = res->ai_addrlen;
+	linebuf->vio->addr.addr = res->ai_addr;
+	linebuf->vio->addr.addrlen = res->ai_addrlen;
 	if (mowgli_vio_connect(linebuf->vio) != 0)
 		return NULL;
 
