752af47e1db542fca33d6de04f55de28f1c3e260
William Pitcock <nenolod@dereferenced.org>
2012-04-01 19:24:49 -0500

    mowgli_strlcpy/strlcat: use libbsd implementation

--- src/libmowgli/core/mowgli_string.c
+++ src/libmowgli/core/mowgli_string.c
@@ -86,38 +86,73 @@ void mowgli_string_append_char(mowgli_string_t *self, const char c)
 	self->str[self->pos] = 0;
 }
 
-/* These functions are taken from Linux. */
-size_t mowgli_strlcat(char *dest, const char *src, size_t count)
+
+/*
+ * Copyright (c) 1998 Todd C. Miller <Todd.Miller@courtesan.com>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+size_t mowgli_strlcat(char *dst, const char *src, size_t siz)
 {
-	size_t dsize = strlen(dest);
-	size_t len = strlen(src);
-	size_t res = dsize + len;
+	char *d = dst;
+	const char *s = src;
+	size_t n = siz;
+	size_t dlen;
 
-	dest += dsize;
-	count -= dsize;
+	/* Find the end of dst and adjust bytes left but don't go past end */
+	while (n-- != 0 && *d != '\0')
+		d++;
+	dlen = d - dst;
+	n = siz - dlen;
 
-	if (len >= count)
-		len = count - 1;
+	if (n == 0)
+		return (dlen + strlen(s));
+	while (*s != '\0') {
+		if (n != 1) {
+			*d++ = *s;
+			n--;
+		}
+		s++;
+	}
+	*d = '\0';
 
-	memcpy(dest, src, len);
-
-	dest[len] = 0;
-
-	return res;
+	return (dlen + (s - src));	/* count does not include NUL */
 }
 
-size_t mowgli_strlcpy(char *dest, const char *src, size_t size)
+size_t mowgli_strlcpy(char *dst, const char *src, size_t siz)
 {
-	size_t ret = strlen(src);
+	char *d = dst;
+	const char *s = src;
+	size_t n = siz;
 
-	if (size)
-	{
-		size_t len = (ret >= size) ? size - 1 : ret;
-		memcpy(dest, src, len);
-		dest[len] = '\0';
+	/* Copy as many bytes as will fit */
+	if (n != 0) {
+		while (--n != 0) {
+			if ((*d++ = *s++) == '\0')
+				break;
+		}
 	}
 
-	return ret;
+	/* Not enough room in dst, add NUL and traverse rest of src */
+	if (n == 0) {
+		if (siz != 0)
+			*d = '\0';		/* NUL-terminate dst */
+		while (*s++)
+			;
+	}
+
+	return (s - src - 1);	/* count does not include NUL */
 }
 
 ssize_t mowgli_writef(mowgli_descriptor_t fd, const char *fmt, ...)
