2e586140edd66aee069e72f02b7e1b3098b514a0
Elizabeth J. Myers <elizabeth@sporksmoo.net>
2012-04-23 11:50:31 -0500

    vio_openssl: add stubs for default functions when built without OpenSSL support.

--- src/libmowgli/vio/vio.h
+++ src/libmowgli/vio/vio.h
@@ -203,7 +203,13 @@ extern int mowgli_vio_openssl_default_read(mowgli_vio_t *vio, void *buffer, size
 extern int mowgli_vio_openssl_default_write(mowgli_vio_t *vio, const void *buffer, size_t len);
 extern int mowgli_vio_openssl_default_close(mowgli_vio_t *vio);
 #else
-#warning "Mowgli compilied without OpenSSL support; anything depending on stacking on the default OpenSSL ops will likely not compile"
+#define NOSSLSUPPORT { mowgli_log("Attempting to use default OpenSSL op with no SSL support; this will not work!"); return -255; }
+static int mowgli_vio_openssl_default_connect(mowgli_vio_t *vio, mowgli_vio_sockaddr_t *addr) NOSSLSUPPORT
+static int mowgli_vio_openssl_default_listen(mowgli_vio_t *vio, int backlog) NOSSLSUPPORT
+static int mowgli_vio_openssl_default_accept(mowgli_vio_t *vio, mowgli_vio_t *newvio) NOSSLSUPPORT
+static int mowgli_vio_openssl_default_read(mowgli_vio_t *vio, void *buffer, size_t len) NOSSLSUPPORT
+static int mowgli_vio_openssl_default_write(mowgli_vio_t *vio, const void *buffer, size_t len) NOSSLSUPPORT
+static int mowgli_vio_openssl_default_close(mowgli_vio_t *vio) NOSSLSUPPORT
 #endif
 
 extern mowgli_vio_ops_t mowgli_vio_default_ops;
