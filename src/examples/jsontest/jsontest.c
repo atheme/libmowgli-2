6ccd9bf7af6fa552d4c923b525147e40c1a2f281
Alex Iadicicco <alex@theta4.com>
2013-01-19 19:29:13 -0700

    Update jsontest example for the new serialization interface

--- src/examples/jsontest/jsontest.c
+++ src/examples/jsontest/jsontest.c
@@ -1,15 +1,19 @@
 #include <mowgli.h>
 
-mowgli_string_t *serialized;
-
-void dump_json(mowgli_json_t *n)
+void out_string(mowgli_json_output_t *out, const char *str, size_t len)
 {
-	mowgli_string_reset(serialized);
-
-	mowgli_json_serialize_to_string(n, serialized, 1);
-
-	printf("%s\n", serialized->str);
+	fwrite(str, 1, len, stdout);
 }
+
+void out_char(mowgli_json_output_t *out, const char c)
+{
+	fputc(c, stdout);
+}
+
+mowgli_json_output_t out = {
+	.append = out_string,
+	.append_char = out_char,
+};
 
 int main(int argc, char *argv[])
 {
@@ -22,19 +26,17 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-	serialized = mowgli_string_create();
-
 	for (i=1; i<argc; i++)
 	{
 		n = mowgli_json_parse_file(argv[i]);
 
-		if (n != NULL)
-			dump_json(n);
+		if (n != NULL) {
+			mowgli_json_serialize(n, &out, 1);
+			putchar('\n');
+		}
 
 		mowgli_json_decref(n);
 	}
-
-	mowgli_string_destroy(serialized);
 
 	return 0;
 }
