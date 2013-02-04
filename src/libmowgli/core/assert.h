da4d92b36aded7f968f345fff27afbab28f38ffb
Patrick McFarland <diablod3@gmail.com>
2013-02-03 22:52:45 -0500

    Change assert.h to use logger correctly

--- src/libmowgli/core/assert.h
+++ src/libmowgli/core/assert.h
@@ -24,73 +24,23 @@
 #ifndef __MOWGLI_ASSERT_H__
 #define __MOWGLI_ASSERT_H__
 
-extern void mowgli_soft_assert_log(const char *asrt, const char *file, int line, const char *function);
+#define _assert_msg(x) "sssertion '#x' failed."
 
-#ifdef __GNUC__
-
-/*
- * Performs a soft assertion. If the assertion fails, we log it.
- */
-#define soft_assert(x)								        \
-	if (!(x)) { 								        \
-                mowgli_soft_assert_log(#x, __FILE__, __LINE__, __PRETTY_FUNCTION__);    \
+#define soft_assert(x, ...) \
+	if (!(x)) { \
+		mowgli_warning(_assert_msg(x),  ##__VA_ARGS__); \
 	}
 
-/*
- * Same as soft_assert, but returns if an assertion fails.
- */
-#define return_if_fail(x)							        \
-	if (!(x)) { 								        \
-                mowgli_soft_assert_log(#x, __FILE__, __LINE__, __PRETTY_FUNCTION__);    \
-		return;								        \
+#define return_if_fail(x, ...) \
+	if (!(x)) { \
+		mowgli_warning(_assert_msg(x), ##__VA_ARGS__); \
+		return; \
 	}
 
-/*
- * Same as soft_assert, but returns a given value if an assertion fails.
- */
-#define return_val_if_fail(x, y)						        \
-	if (!(x)) { 								        \
-                mowgli_soft_assert_log(#x, __FILE__, __LINE__, __PRETTY_FUNCTION__);    \
-		return (y);							        \
+#define return_val_if_fail(x, y, ...) \
+	if (!(x)) { \
+		mowgli_warning(_assert_msg(x), ##__VA_ARGS__); \
+		return (y); \
 	}
-
-#else
-
-/*
- * Performs a soft assertion. If the assertion fails, we log it.
- */
-#define soft_assert(x)								        \
-	if (!(x)) { 								        \
-		mowgli_soft_assert_log(#x, __FILE__, __LINE__, __FUNCTION__);	        \
-	}
-
-/*
- * Same as soft_assert, but returns if an assertion fails.
- */
-#define return_if_fail(x)							        \
-	if (!(x)) { 								        \
-		mowgli_soft_assert_log(#x, __FILE__, __LINE__, __FUNCTION__);	        \
-		return;								        \
-	}
-
-/*
- * Same as soft_assert, but returns a given value if an assertion fails.
- */
-#define return_val_if_fail(x, y)						        \
-	if (!(x)) { 								        \
-		mowgli_soft_assert_log(#x, __FILE__, __LINE__, __FUNCTION__);	        \
-		return (y);							        \
-	}
-
-/*
- * Same as soft_assert, but returns NULL if the value is NULL.
- */
-#define return_if_null(x)							        \
-	if (x == NULL) { 							        \
-		mowgli_soft_assert_log(#x, __FILE__, __LINE__, __FUNCTION__);	        \
-		return (NULL);							        \
-	}
-
-#endif
 
 #endif
