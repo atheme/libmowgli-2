63191f9a10de2b9075bc80a892c764117fbf63de
Patrick McFarland <diablod3@gmail.com>
2013-02-14 16:08:07 -0500

    Add cacheline routines

---
+++ src/libmowgli/platform/cacheline.c
@@ -0,0 +1,61 @@
+/*
+ * libmowgli: A collection of useful routines for programming.
+ * cacheline.c: Platform specific routines to get L1 cache line size
+ *
+ * Copyright (c) 2013 Patrick McFarland <pmcfarland@adterrasperaspera.com>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice is present in all copies.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "mowgli.h"
+
+size_t _mowgli_cacheline_size;
+
+void mowgli_cacheline_init(void) {
+#ifdef MOWGLI_OS_LINUX
+	_mowgli_cacheline_size = sysconf(_SC_LEVEL1_DCACHE_LINESIZE);
+#elif MOWGLI_OS_OSX
+	sysctlbyname("hw.cachelinesize", &_mowgli_cacheline_size, 
+			sizeof(size_t), 0, 0);
+#elif MOWGLI_OS_WIN
+#define SLPI SYSTEM_LOGICAL_PROCESSOR_INFORMATION
+	DWORD buf_size = 0;
+	DWORD i = 0;
+	SLPI *buf = 0;
+
+	GetLogicalProcessorInformation(0, &buf_size);
+	buf = (SLPI *)atheme_alloc(buf_size);
+	GetLogicalProcessorInformation(&buf[0], &buf_size);
+
+	for (i = 0; i != buf_size / sizeof(SLPI); ++i) {
+		if (buf[i].Relationship == RelationCache && buf[i].Cache.Level == 1) {
+			_mowgli_cacheline_size = buf[i].Cache.LineSize;
+			break;
+		}
+	}
+
+  atheme_free(buffer);
+#undef SLPI
+#else
+	// This is often true
+#ifdef MOWGLI_CPU_BITS_32
+	_mowgli_cacheline_size = 32;
+#else
+	_mowgli_cacheline_size = 64;
+#endif
+#endif
+}
---
+++ src/libmowgli/platform/cacheline.h
@@ -0,0 +1,28 @@
+/*
+ * libmowgli: A collection of useful routines for programming.
+ * cacheline.h: Platform specific routines to get L1 cache line size
+ *
+ * Copyright (c) 2013 Patrick McFarland <pmcfarland@adterrasperaspera.com>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice is present in all copies.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+extern size_t _mowgli_cacheline_size;
+
+static inline size_t mowgli_cacheline_size(void) {
+	return _mowgli_cacheline_size;
+}
